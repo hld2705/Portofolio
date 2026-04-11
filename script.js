bb5867220f972ba2f36a319aad1cd30754e87667
Halid Crnkić <halidcrnkic@gmail.com>
2026-04-12 01:03:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function init(){
+    languageElipse()
+}
+
+function languageElipse(){
+    let de = document.getElementById("de");
+    let en = document.getElementById("en");
+    let img = document.createElement('img');
+    img.src = "./img/Ellipse 24.svg"
+    if(de){
+        img
+    }
+}
